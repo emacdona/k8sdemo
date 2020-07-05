d531bf2b0339957caa61d863d32739f83273fe80
Ed MacDonald <edmacdonald555@gmail.com>
2020-07-05 19:31:20 -0400

    reordering demo scripts

--- demo-script/07-showPersistentHostnameForStatefulSet.sh
+++ demo-script/07-showPersistentHostnameForStatefulSet.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-POD_NAME=$(kubectl get pods -l name=confusedapp -o json | jq -r '.items[0].metadata.name')
+POD_NAME=$(kubectl get pods -l name=statelessapp -o json | jq -r '.items[0].metadata.name')
 
 kubectl exec --stdin --tty $POD_NAME -- /bin/sh -c 'hostname && ping -c 4 statefulapp-0.statefulapp.default.svc.cluster.local'
