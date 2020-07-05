c4f7bea323bd3b867c21cbeb201ba7f515f1491c
Ed MacDonald <edmacdonald555@gmail.com>
2020-07-05 19:27:00 -0400

    This works on my linux machine... not on my mac, though... need to figure out why

--- demo-script/09-showPersistentHostnameForStatefulSet.sh
+++ demo-script/09-showPersistentHostnameForStatefulSet.sh
@@ -3,4 +3,4 @@ set -eo pipefail
 
 POD_NAME=$(kubectl get pods -l name=confusedapp -o json | jq -r '.items[0].metadata.name')
 
-kubectl exec --stdin --tty statefulapp-0 -- ping statefulapp-0.statefulapp.default.svc.cluster.local
+kubectl exec --stdin --tty $POD_NAME -- /bin/sh -c 'hostname && ping -c 4 statefulapp-0.statefulapp.default.svc.cluster.local'
