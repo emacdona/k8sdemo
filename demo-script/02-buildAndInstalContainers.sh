0c93cc1e1b07d2e03465e6cb47eed4014da1a12f
Ed MacDonald <emacdonald@solutionstreet.com>
2020-07-06 09:24:54 -0400

    Modified these so that they could be run regardless of current working directory

--- demo-script/02-buildAndInstalContainers.sh
+++ demo-script/02-buildAndInstalContainers.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-(  cd simpleapp && \
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+(  cd ${DIR}/../simpleapp && \
    ./gradlew clean build && \
    docker build --build-arg JAR_FILE=build/libs/simpleapp-0.0.1-SNAPSHOT.jar -t kubedemo/statelessapp .\
 )
 
-(  cd statefulapp && \
+(  cd ${DIR}/../statefulapp && \
    ./gradlew clean build && \
    docker build --build-arg JAR_FILE=build/libs/statefulapp-0.0.1-SNAPSHOT.jar -t kubedemo/statefulapp .
 )
